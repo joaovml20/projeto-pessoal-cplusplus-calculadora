8be5541992a1aebe51f2a8720a3c7617dc389c61
joaovml20 <joaolima15257@gmail.com>
2026-06-16 20:56:56 -0300

    colocando o programa em um do-while

--- main.cpp
+++ main.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
-using namespace std;
 
 int main(){
     int resposta;
     double numero1,numero2;
-    cout<<"bem-vindo a calculadora, para comecar, digite o primeiro numero: ";
-    cin>>numero1;
-    cout<<"agora digite o segundo numero: ";
-    cin>>numero2;
-    cout<<"agora escolhar a operacacao a ser realizada entre os dois numeros: 1 para somar, 2 para subtrair, 3 para dividir, 4 para multiplicar: ";
-    cin>>resposta;
-    switch(resposta){
-        case 1:
-            cout<<"resultado da operacao: "<<numero1<<" + "<<numero2 << " = " <<numero1 + numero2 << endl;
-            break;
-        case 2:
-            cout<<"resultado da operacao: "<<numero1<<" - "<<numero2 << " = " <<numero1 - numero2 << endl;
-            break;
-        case 3:
-            if(numero2 == 0){
-                cout<<"impossivel dividir por zero"<<endl;
-            } else {
-                cout<<"resultado da operacao: "<<numero1<<" / "<<numero2 << " = " <<numero1 / numero2 << endl;
+    do{
+        std::cout<<"bem-vindo a calculadora, agora escolhar a operacacao a ser realizada: 0 para fechar a calculadora 1 para somar, 2 para subtrair, 3 para dividir, 4 para multiplicar: ";
+        std::cin>>resposta;
+        switch(resposta){
+            case 0:
+                std::cout<<"fechando a calculadora..."<<std::endl;
+                break;
+            case 1:
+                std::cout<<"informe o valor do primeiro numero: ";
+                std::cin>>numero1;
+                std::cout<<"agora informe o segundo numero: ";
+                std::cin>>numero2;
+                std::cout<<"resultado da operacao: "<<numero1<<" + "<<numero2 << " = " <<numero1 + numero2 << std::endl;
+                break;
+            case 2:
+                std::cout<<"informe o valor do primeiro numero: ";
+                std::cin>>numero1;
+                std::cout<<"agora informe o segundo numero: ";
+                std::cin>>numero2;
+                std::cout<<"resultado da operacao: "<<numero1<<" - "<<numero2 << " = " <<numero1 - numero2 << std::endl;
+                break;
+            case 3:
+                std::cout<<"informe o valor do primeiro numero: ";
+                std::cin>>numero1;
+                std::cout<<"agora informe o segundo numero: ";
+                std::cin>>numero2;
+                if(numero2 == 0){
+                    std::cout<<"impossivel dividir por zero"<<std::endl;
+                } else {
+                    std::cout<<"resultado da operacao: "<<numero1<<" / "<<numero2 << " = " <<numero1 / numero2 << std::endl;
+                }
+                break;
+            case 4:
+                std::cout<<"informe o valor do primeiro numero: ";
+                std::cin>>numero1;
+                std::cout<<"agora informe o segundo numero: ";
+                std::cin>>numero2;
+                std::cout<<"resultado da operacao: "<<numero1<<" * "<<numero2 << " = " <<numero1 * numero2 << std::endl;
+                break;
+            default:
+                std::cout<<"opcao de operacao nao encontradar"<<std::endl;
+                break;
             }
-            break;
-        case 4:
-            cout<<"resultado da operacao: "<<numero1<<" * "<<numero2 << " = " <<numero1 * numero2 << endl;
-            break;
-        default:
-            cout<<"opcao de operacao nao encontradar";
-            break;
-    }
+        if(resposta != 0)
+            std::cout<<"realizando outra operacão..."<<std::endl;
+    } while(resposta != 0);
     return 0;
 }
